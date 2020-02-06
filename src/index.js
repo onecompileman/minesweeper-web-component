147ae2480a7723607f8a027e187db92ea24f9177
onecompileman <stephen.vinuya@gmail.com>
2020-02-06 15:24:17 +0800

    feat: adjusted webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './components/tile';
 import './components/game-manager';
+import './assets/css/style.css';
+import './assets/images/bomb.png';
+import './assets/images/time.png';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +7,26 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/images/[name].[ext]'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
